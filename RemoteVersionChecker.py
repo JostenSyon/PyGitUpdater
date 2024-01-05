e39f11f0a0d8cc641ff3f6511ede7a18a8911427
JostenSyon <jostensyon@gmail.com>
2024-01-06 00:26:23 +0100

    this class check on the Github link provided and return the latest version as String, can also remove a prefix like ver or v from the tag

---
+++ RemoteVersionChecker.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+class RemoteVersionChecker:
+    def __init__(self, github_url, prefix=None):
+        self.github_url = github_url
+        self.prefix = prefix
+
+    def get_version(self):
+        # Get the latest version available
+        response = requests.get(self.github_url)
+        data = json.loads(response.text)
+        remote = data['tag_name']
+        if self.prefix:
+            remote = remote.lstrip(self.prefix)  # Remove the prefix
+        return remote
+
+# Usage
+#version_checker = RemoteVersionChecker("https://api.github.com/repos/JostenSyon/GUI4GL-2023/releases/latest", "v")
+#print(version_checker.get_version())
